ec84ae990d25046411c3d3fda50111c3e3caad7a
HSRai <hardeep.rai@gmail.com>
2025-08-09 16:28:44 +0530

    qe, live result on user input

--- pages/qe.vue
+++ pages/qe.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
-    The sum of {{ result.a }} and {{ result.b }} is <strong>{{ result.sum }}</strong>.
+    <label>
+      A: <input type="number" v-model.number="a" />
+    </label>
+    <label>
+      B: <input type="number" v-model.number="b" />
+    </label>
+    <div style="margin-top: 1em;">
+      The sum of {{ result.a }} and {{ result.b }} is <strong>{{ result.sum }}</strong>.
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import { useQe } from '@/composables/useQe'
-const result = useQe(10, 3)
+
+const a = ref(10)
+const b = ref(3)
+const result = computed(() => useQe(a.value, b.value))
 </script>
