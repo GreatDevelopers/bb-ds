c141c1abb78f36b79988ab688a106ef8c87f9b36
HSRai <hardeep.rai@gmail.com>
2025-08-10 14:35:45 +0530

    ifc.vue from pages removed

--- pages/ifc.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <IfcViewer />
-  <!-- In future: creation and editing UI goes here -->
-</template>
-
-<script setup lang="ts">
-import IfcViewer from '@/components/IfcViewer.vue'
-</script>
